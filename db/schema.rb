0fe52bbfc3fc8f32b152c4b5c48a128d786156ab
Yaeseul Lee <skghfe@hanmail.net>
2016-08-25 13:44:00 +0900

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160816061833) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
     t.text     "ingredient"
@@ -38,8 +41,8 @@ ActiveRecord::Schema.define(version: 20160816061833) do
     t.string   "uid"
     t.string   "name"
     t.string   "image"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
